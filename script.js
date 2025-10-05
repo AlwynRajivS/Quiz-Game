042c969fabaa6d368bb381967713596f3737aca0
AlwynRajivS <119982824+AlwynRajivS@users.noreply.github.com>
2025-10-05 18:15:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // This script handles Host & Player interactions
 // Placeholder for live Google Sheet API URL
-const API_URL = "https://script.google.com/macros/s/AKfycbz_MCD_VTjQsvVR0g3Mn2gtMRAbP-4i0XLeGj2nG1XHObu3v8oj2Mxa8M5IQwv9I9XD/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbymLXRPkw3sdMmb5vq96v0lehR5YjxTOSZ3VjgAL87hL0_siVkRXq54lfWtK8_y6YKu/exec";
 
 // You can copy all JS logic from the previous code provided
+
